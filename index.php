58e8d590e90f73e4eeeeef8714fed62a93edd909
muneebkattody <muneebkattody@users.noreply.github.com>
2019-06-15 17:25:35 +0530

    index

--- index.php
+++ index.php
@@ -2,12 +2,9 @@
 $title = "Dissaster Management Software";
 require('header.php');
 ?>
-<link rel="stylesheet" type="text/css" href="css/w3.css">
 </head>
 <body>
-<div class="w3-container w3-red">
 <h1>Disaster Management Software</h1>
-</div>
 <a href="campneeds.php">Camp Needs Form</a><br>
 <a href="donation.php">Donation Form</a><br>
 <a href="analysis.php">Analysis</a><br>
